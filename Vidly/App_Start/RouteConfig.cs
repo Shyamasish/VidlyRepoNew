9147bf7898780fd1836865bfba16a3bc3e10779e
Shyamasish Mohanty <shyamasish@outlook.com>
2019-04-19 12:01:53 +0530

    bknj

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,17 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes(); //to activate attribute based routing
+
+            //// New cutom route created for study, We should have the order of the route from morespecific to more generic
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}", //we can craete a user defined url pattern unlike controller/action etc done in default route
+            //    new {Controller= "Movies", Action ="ByReleaseDate"},
+            //    new {year = @"\d{4}", month =@"\d{2}"} // here we added this new line of code to explain that we can control the pattern of parameters.
+            //    //new {year = @"2018|2019"} // similarly we can restrit theparameter to some specific values
+            //    );
+            // /////////////////////////////////////////////////////////////////////////
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+            var viewmodel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            //using viewdata
+            //ViewData["Movie"] = movie;
+            return View(viewmodel);
+        }
+        //[Route("movies/released/{year}/{month:regex(\\d[4}):range(1, 12)}")] //[vid-13]This is attribute based routing regex is to add ergualr expression range is to add the range of month. they are optional and many more constraints supproted by mvc framework
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+    }
+}
