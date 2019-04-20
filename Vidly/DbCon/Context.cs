c4e1b8db58949a5d2b1dbb076afd10230f57a8cf
shyamasish <shyamasishmohanty@gmail.com>
2019-04-20 09:52:18 +0530

    jn

---
+++ Vidly/DbCon/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.DbCon
+{
+    public class Context: DbContext
+    {
+        public Context()
+        {
+        }
+    }
+}
